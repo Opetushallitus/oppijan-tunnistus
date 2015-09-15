9889ebf68e4d5d65dfa5f0ef184220efa7fb4565
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-09-15 16:58:27 +0300

    SQL statements

---
+++ resources/sql/get.sql
@@ -0,0 +1,1 @@
+select * from secure_link where token = :token
--- resources/sql/insert.sql
+++ resources/sql/insert.sql
@@ -1,1 +1,2 @@
-select * from securelink
+INSERT INTO secure_link (id, valid_until, email, token, callback_url)
+VALUES (nextval('secure_link_id_seq'), :valid_until::timestamptz, :email, :token, :callback_url);
