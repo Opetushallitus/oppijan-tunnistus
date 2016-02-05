44383a91564e6337c6d8ac93b0eb18a6eb2d4224
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-02-05 09:06:54 +0200

    BUG-716 sending email behind firewall without cas

--- spec/fi/vm/sade/oppijantunnistus/fake_server.clj
+++ spec/fi/vm/sade/oppijantunnistus/fake_server.clj
@@ -1,4 +1,4 @@
-(ns fi.vm.sade.oppijantunnistus.fake_cas_server
+(ns fi.vm.sade.oppijantunnistus.fake_server
   (:require [ring.middleware.json :refer [wrap-json-response wrap-json-params]]
             [ring.util.response :refer [response created header set-cookie]]
             [ring.util.http-response :refer [ok internal-server-error! internal-server-error]]
@@ -12,39 +12,32 @@
             [ring.adapter.jetty :refer [run-jetty]]))
 
 (def port
-  (-> (url (-> cfg :cas :url))
+  (-> (url (-> cfg :ryhmasahkoposti :url))
       (get :port)))
-
-(def cas_url (str "http://localhost:" port))
 
 (def ^:private server_on (atom true))
 
 (defn enable_server [enable?] (reset! server_on enable?))
 
-(defroutes* ryhmasahkoposti_cas_routes
-            (POST "/cas/v1/tickets" [] (created (str cas_url "/cas/v1/tickets/TGT-123")))
-            (POST "/cas/v1/tickets/TGT-123" [] (ok "ST-123"))
-            (POST "/ryhmasahkoposti-service/email" {body :body} (if @server_on
+(defroutes* ryhmasahkoposti_routes
+            (POST "/ryhmasahkoposti-service/email/firewall" {body :body} (if @server_on
                                                                   (let [message (read-str (slurp body))]
                                                                     (log/info "Ryhmasahkoposti Received Message" message)
                                                                     (if (not (clojure.string/blank? ((message "email") "body")))
                                                                       (-> (ok) (header "Content-Type" "application/json;charset=utf-8"))
                                                                       (-> (internal-server-error!) (header "Content-Type" "application/json;charset=utf-8"))))
-                                                      (-> (internal-server-error) (header "Content-Type" "application/json;charset=utf-8"))))
-            (GET "/ryhmasahkoposti-service/j_spring_cas_security_check" []
-              (-> (ok)
-                  (header "Set-Cookie" "JSESSIONID=foobar-123"))))
+                                                      (-> (internal-server-error) (header "Content-Type" "application/json;charset=utf-8")))))
 
-(defapi fake_cas_server
-        ryhmasahkoposti_cas_routes)
+(defapi fake_server
+        ryhmasahkoposti_routes)
 
-(defonce ^:dynamic *cas_app* (atom nil))
+(defonce ^:dynamic *fake_app* (atom nil))
 
-(defn start-cas-app []
-  (log/info "Starting fake ryhmasahkoposti with mocked CAS in URL" cas_url)
-  (if (not (nil? @*cas_app*))
-    (.stop @*cas_app*))
-  (reset! *cas_app* (run-jetty fake_cas_server {:port port :join? false} )))
+(defn start-fake-app []
+  (log/info "Starting fake ryhmasahkoposti in URL" (-> cfg :ryhmasahkoposti :url))
+  (if (not (nil? @*fake_app*))
+    (.stop @*fake_app*))
+  (reset! *fake_app* (run-jetty fake_server {:port port :join? false} )))
 
-(defn stop-cas-app []
-  (.stop @*cas_app*))
+(defn stop-fake-app []
+  (.stop @*fake_app*))
