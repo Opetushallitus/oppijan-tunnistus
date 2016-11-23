691147f17f8a100a67bfa3c7e763602af858fa7e
Tommi Helander <tommi.helander@gofore.com>
2016-11-23 14:33:44 +0200

    Fix NullpointerException when compiling uberjar

--- src/fi/vm/sade/oppijantunnistus/url_helper.clj
+++ src/fi/vm/sade/oppijantunnistus/url_helper.clj
@@ -2,12 +2,13 @@
   (:require [fi.vm.sade.oppijantunnistus.config :refer [cfg]])
   (:import [fi.vm.sade.properties OphProperties]))
 
-(defonce oph-properties
-  (let [host-virkailija (:host-virkailija cfg)]
-    (doto (OphProperties. (into-array String ["/oppijan-tunnistus-oph.properties"]))
-          (.addDefault "url-virkailija" host-virkailija))))
+(when-not *compile-files*
+  (defonce oph-properties
+    (let [host-virkailija (:host-virkailija cfg)]
+      (doto (OphProperties. (into-array String ["/oppijan-tunnistus-oph.properties"]))
+            (.addDefault "url-virkailija" host-virkailija)))))
 
 (defn url
-  ([key] (url key [])) 
+  ([key] (url key []))
   ([key params]
     (.url oph-properties (name key) (to-array params))))
