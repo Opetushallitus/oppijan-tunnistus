b15c2a48e0d9f7e6bab747693ab3f445c90f1a15
Tommi Helander <tommi.helander@gofore.com>
2016-11-24 13:13:42 +0200

    Make url-helper stateless

--- src/fi/vm/sade/oppijantunnistus/url_helper.clj
+++ src/fi/vm/sade/oppijantunnistus/url_helper.clj
@@ -2,13 +2,12 @@
   (:require [fi.vm.sade.oppijantunnistus.config :refer [cfg]])
   (:import [fi.vm.sade.properties OphProperties]))
 
-(when-not *compile-files*
-  (defonce oph-properties
-    (let [host-virkailija (:host-virkailija cfg)]
-      (doto (OphProperties. (into-array String ["/oppijan-tunnistus-oph.properties"]))
-            (.addDefault "url-virkailija" host-virkailija)))))
+(defn oph-properties []
+  (let [host-virkailija (:host-virkailija cfg)]
+    (doto (OphProperties. (into-array String ["/oppijan-tunnistus-oph.properties"]))
+          (.addDefault "url-virkailija" host-virkailija))))
 
 (defn url
   ([key] (url key []))
   ([key params]
-    (.url oph-properties (name key) (to-array params))))
+    (.url (oph-properties) (name key) (to-array params))))
